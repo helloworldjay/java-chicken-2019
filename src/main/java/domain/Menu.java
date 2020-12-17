f81fe745fa5e0dd0194d5324c5c5789473b9b8b7
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-12-17 22:05:54 +0900

    refactor: Menu에 기능 추가

--- src/main/java/domain/Menu.java
+++ src/main/java/domain/Menu.java
@@ -1,10 +1,12 @@
 package domain;
 
 public class Menu {
+
     private final int number;
     private final String name;
     private final Category category;
     private final int price;
+    private int orderNumber = 0;
 
     public Menu(final int number, final String name, final Category category, final int price) {
         this.number = number;
@@ -16,5 +18,32 @@ public class Menu {
     @Override
     public String toString() {
         return category + " " + number + " - " + name + " : " + price + "원";
+    }
+
+    public boolean isChicken() {
+        if (category.toString().equals("[치킨]")) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getPriceMessage() {
+        return name + " " + Integer.toString(orderNumber) + " "+ Integer.toString(orderNumber * price);
+    }
+
+    public int getPrice() {
+        return orderNumber * price;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public void orderByNumber(int orderNumber) {
+        this.orderNumber += orderNumber;
+    }
+
+    public int getOrderNumber() {
+        return this.orderNumber;
     }
 }
