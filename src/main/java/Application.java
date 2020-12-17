7f09bb1cdd9b6c5e61a78e502040a75ff1d079df
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-12-17 22:04:42 +0900

    feat: 기본 실행 Application 설정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,21 +1,13 @@
-import domain.Menu;
-import domain.MenuRepository;
+import Controller.MainSelection;
 import domain.Table;
 import domain.TableRepository;
-import view.InputView;
-import view.OutputView;
-
 import java.util.List;
+import view.InputView;
 
 public class Application {
     // TODO 구현 진행
     public static void main(String[] args) {
         final List<Table> tables = TableRepository.tables();
-        OutputView.printTables(tables);
-
-        final int tableNumber = InputView.inputTableNumber();
-
-        final List<Menu> menus = MenuRepository.menus();
-        OutputView.printMenus(menus);
+        MainSelection.selectMain(tables);
     }
 }
