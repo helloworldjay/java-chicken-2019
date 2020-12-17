4984b8e6dfd0f003bb2fa416e9619f17a8f3ec15
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-12-17 22:06:48 +0900

    refactor: Table에 method 및 field 추가

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -1,7 +1,11 @@
 package domain;
 
+
 public class Table {
+
     private final int number;
+    private boolean isPaid = true;
+    private MenuRepository tableOrderList = new MenuRepository();
 
     public Table(final int number) {
         this.number = number;
@@ -11,4 +15,28 @@ public class Table {
     public String toString() {
         return Integer.toString(number);
     }
+
+    public void isNotPaid() {
+        this.isPaid = false;
+    }
+
+    public boolean checkPayment() {
+        return this.isPaid;
+    }
+
+    public void addOrder(int menuNumber, int orderAmount) {
+        Menu menu = tableOrderList.getMenuByNumber(menuNumber);
+        menu.orderByNumber(orderAmount);
+    }
+
+    public void clearTable() {
+        tableOrderList = new MenuRepository();
+        isPaid = true;
+    }
+
+    public MenuRepository getTableOrderList() {
+        return tableOrderList;
+    }
+
+
 }
