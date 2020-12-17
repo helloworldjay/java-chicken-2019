426f329815fac43f20bab95536de8714e33457e4
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-12-17 22:07:19 +0900

    refactor: Table Repo에 Field 및 Method 추가

--- src/main/java/domain/TableRepository.java
+++ src/main/java/domain/TableRepository.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class TableRepository {
+
     private static final List<Table> tables = new ArrayList<>();
 
     static {
@@ -18,5 +19,14 @@ public class TableRepository {
 
     public static List<Table> tables() {
         return Collections.unmodifiableList(tables);
+    }
+
+    public static Table getTable(int number) {
+        for (int i = 0; i < tables.size(); i++) {
+            if (tables.get(i).toString().equals(Integer.toString(number))) {
+                return tables.get(i);
+            }
+        }
+        return new Table(-1); // 처리 방법이 애매
     }
 }
